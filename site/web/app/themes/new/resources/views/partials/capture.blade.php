a6cc2ed471ea724f0f577781bc23174f62ca9717
Ben <benjitsu@users.noreply.github.com>
2019-04-12 18:24:24 -0700

    updates

--- site/web/app/themes/new/resources/views/partials/capture.blade.php
+++ site/web/app/themes/new/resources/views/partials/capture.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
 <div class="row">
 <div class="box col-lg-10 mr-auto ml-auto text-center">
-<span class="headline">Want to train Brazilian Jiu-Jitsu? Get your free trial class!</span>
+<span class="headline">Want to start training? Get a free trial class!</span>
 @php echo do_shortcode('[contact-form-7 id="66" title="capture"]') @endphp
 </div>
 </div>
--- site/web/app/themes/new/resources/views/template-contact.blade.php
+++ site/web/app/themes/new/resources/views/template-contact.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="row mt-5 mb-5">
     <div class="col-lg-12">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.301714851864!2d-122.28095048413368!3d37.85323021541371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857e629c864151%3A0x40a5f9f069ed1de5!2sRalph+Gracie+Berkeley+Jiu+Jitsu+Academy!5e0!3m2!1sen!2sus!4v1546228106490"
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6303.174824079996!2d-122.28159600000001!3d37.823132!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857e1a143d9c8f%3A0x386861d87d00c367!2sDogtown+Athletic!5e0!3m2!1sen!2sus!4v1555117549164!5m2!1sen!2sus"
                         width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
     </div>
@@ -23,13 +23,13 @@
 </div>
   @endwhile
   <div class="col-lg-4 d-flex justify-content-center align-items-center">
-  Ralph Gracie Jiu-Jitsu Academy<br />
-                        1500 Ashby Ave<br />
-                        Berkeley, CA 94703<br /><br /><br/>
+  Dogtown Athletic<br/>
+                        3109 Adeline St<br />
+                        Oakland, CA 94608<br /><br /><br/>
     
     
-                        (510) 486-8000<br />
-                        info [at] ralphgracieberkeley.com<br/><br/>
+                        (510) 328-3644<br />
+                        info [at] dogtownathletic.com
   </div>
 </div>
 
