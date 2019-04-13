cacc276037007c10389baed1540023c52020bff8
Ben <benjitsu@users.noreply.github.com>
2019-04-12 18:37:05 -0700

    update

--- site/web/app/themes/new/resources/views/partials/footer.blade.php
+++ site/web/app/themes/new/resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="content-info mt7">
   <div class="container">
     <div class="row justify-content-center">
-    <a href="#"><i class="fab fa-instagram"></i></a>
-    <a href="#"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.instagram.com/dogtownathletic/" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/dogtownathletic" target="_blank"><i class="fab fa-facebook-f"></i></a>
     </div>
-    <div class="row justify-content-center">© Ralph Gracie Berkeley Brazilian Jiu-Jitsu Academy</div>
+    <div class="row justify-content-center">© 2019 Dogtown Athletic</div>
   </div>
 </footer>
