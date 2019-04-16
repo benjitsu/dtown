4b91716e06f416c918711b6927d82569a9d8942d
Ben <benjitsu@users.noreply.github.com>
2019-04-15 22:52:24 -0700

    updates

--- site/web/app/themes/new/resources/views/front-page.blade.php
+++ site/web/app/themes/new/resources/views/front-page.blade.php
@@ -15,37 +15,37 @@
                 <h2 class="mr-auto ml-auto">Our Programs</h2>
             </div>
             <div class="row">
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                     <a href="/metcon">
                     <div class="program metcon"></div>
                     <div class="name">Metcon</div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                 <a href="/metcon">
                     <div class="program strength"></div>
                     <div class="name">Strength & Conditioning</div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                 <a href="/metcon">
                     <div class="program functional"></div>
                     <div class="name">Functional Grind</div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                 <a href="/metcon">
                     <div class="program personal"></div>
                     <div class="name">Personal Training</div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                 <a href="/metcon">
                     <div class="program bjj"></div>
                     <div class="name">Brazilian Jiu-Jitsu</div>
                     </a>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-md-6">
                 <a href="/metcon">
                     <div class="program muaythai"></div>
                     <div class="name">Muay Thai</div>
@@ -83,7 +83,7 @@ That is our Mission at Dogtown Athletic. It does not mean we only want “athlet
 <p>But not just athletic… Dogtown Athletic.</p>
 
                     </div>
-                    <div class="col-lg-4 offset-lg-1 mt-md-3">
+                    <div class="col-lg-4 offset-lg-1 mt-md-4 mt-sm-4">
                         <div class="sideImg"></div>
                     </div>
                 </div>
@@ -113,11 +113,11 @@ That is our Mission at Dogtown Athletic. It does not mean we only want “athlet
                 <h2 class="mr-auto ml-auto">Come Meet Us</h2>
             </div>
             <div class="row">
-                <div class="col-8">
+                <div class="col-lg-8 order-2 order-lg-1 mt-4 mt-lg-0">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6303.174824079996!2d-122.28159600000001!3d37.823132!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857e1a143d9c8f%3A0x386861d87d00c367!2sDogtown+Athletic!5e0!3m2!1sen!2sus!4v1555117549164!5m2!1sen!2sus"
                         width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
-                <div class="col-4 details d-flex justify-content-center align-items-center ">
+                <div class="col-lg-4 details d-flex justify-content-lg-center align-items-lg-center order-1 order-lg-2 ">
                    <div class="mb-2">
                         Dogtown Athletic<br/>
                         3109 Adeline St<br />
