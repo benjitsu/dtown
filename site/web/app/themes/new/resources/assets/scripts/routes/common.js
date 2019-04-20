6100c7c2efbaa7523cfb5780e2bdd0910ffe029e
Ben <benjitsu@users.noreply.github.com>
2019-04-20 01:53:56 -0700

    hamburger

--- site/web/app/themes/new/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/new/resources/assets/scripts/routes/common.js
@@ -1,6 +1,10 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.hamburger').click(function() {
+      $(this).toggleClass('is-active');
+      $('.navbar-collapse').toggleClass('show');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- site/web/app/themes/new/resources/views/partials/header.blade.php
+++ site/web/app/themes/new/resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header class="banner">
     <nav class="navbar navbar-expand-lg navbar-dark bg-faded">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
- data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
- aria-label="Toggle navigation">
- <span class="navbar-toggler-icon"></span>
- </button>
+    <button class="hamburger hamburger--vortex d-lg-none" type="button">
+  <span class="hamburger-box">
+    <span class="hamburger-inner"></span>
+  </span>
+</button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
     @if (has_nav_menu('primary_navigation'))
   {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav ml-auto', 'walker' => new \App\wp_bootstrap4_navwalker()]) !!}
